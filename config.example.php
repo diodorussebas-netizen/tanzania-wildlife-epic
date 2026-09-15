e3a067451d1ab6b3be3773b69c478a31a742feae
diodorussebas-netizen <diodorussebas@gmail.com>
2026-09-15 17:55:12 +0300

    Add verified site configuration example

---
+++ config.example.php
@@ -0,0 +1,13 @@
+<?php
+return [
+  'db'=>['host'=>'localhost','name'=>'twe_cms','user'=>'root','pass'=>''],
+  'site'=>[
+    'name'=>'Tanzania Wildlife Epic',
+    'url'=>'https://tanzaniawildlifeepic.com',
+    'phone'=>'+255 747 304 628',
+    'whatsapp'=>'255747304628',
+    'email'=>'info@tanzaniawildlifeepic.com',
+    'email2'=>'tanzaniawildlifeepic@gmail.com',
+    'location'=>'Kilimanjaro Region / Arusha Road, Tanzania'
+  ]
+];
